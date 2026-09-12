4f7c74c28776cff9501bd7dad06981c416e007bd
Abdurraouf <a.k.sadi80@gmail.com>
2026-09-11 18:19:39 -0700

    Update backend

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ const bookingRoutes = require("./routes/bookingRoutes");
 const AdminRoutes = require("./routes/adminRoutes");
 const databaseRoutes = require("./routes/databaseRoutes");
 const hostBlockedUserRoutes = require("./routes/hostBlockedUserRoutes");
+const stateRoutes = require("./routes/statsRoutes")
 const app = express();
 const logger = require("./middleware/logger");
 
@@ -110,6 +111,8 @@ app.use("/api/v1/dashboard", AdminRoutes);
 app.use("/api/v1/uploads", uploadRoutes);
 app.use("/api/v1/database", databaseRoutes);
 app.use("/api/v1/host", hostBlockedUserRoutes);
+app.use("/api/v1/state", stateRoutes);
+
 // Root route
 app.get("/", (req, res) => {
   res.status(200).json({
