65160cd2a0f922b98c3439127c31ff26af8e0210
Abdurraouf <a.k.sadi80@gmail.com>
2026-09-11 21:14:21 -0700

    Update backend

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -77,7 +77,7 @@ const createBooking = asyncHandler(async (req, res) => {
 
   const blockedUser = await prisma.$queryRaw`
   SELECT id
-  FROM host_blocked_users
+  FROM HostBlockedUser
   WHERE host_id = ${listing.host_id}
     AND user_id = ${userId}
   LIMIT 1
